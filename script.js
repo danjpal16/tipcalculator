cfae1381e316840e6e570ca74fc01a9fd4fc43ef
danjpal16 <525dpalagano@frhsd.com>
2024-01-23 14:55:21 -0500

    more java

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 //Function to calculate and display tip, total bill, and amount per person
 function calculateAndDisplay(){
-    
+    // Get the value from the HTML form
+    let bill = parseFloat(document.getElementById('totalBill').value)
+    let numOfPeople = parseInt(document.getElementById('numOfPeople').value)
+    let serviceQuality = document.getElementById('serviceQuality').value
+
+    // calculate tip, total bill, and the amount each person owes
+
+    let tip = calculateTip(bill, serviceQuality)
+    let totalBill = calculateTotalBill(bill, tip)
+    let amtPerPers = calcAmtPerPers(totalBill, numOfPeople)
+
+    // Display the Results in the Html
+
+    document.getElementById('tipResult').innerText = 'Tip: $' + tip.toFixed(2)
+    document.getElementById('totalBillResult').innerText = 'Tip: $' + totalBill.toFixed(2)
+    document.getElementById('amtPerPersonResult').innerText = 'Tip: $' + amtPerPers.toFixed(2)
 }
