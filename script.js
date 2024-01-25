653c734423f2990ed747398bf18cfed88504caea
danjpal16 <525dpalagano@frhsd.com>
2024-01-25 13:48:22 -0500

    finished tutorial

--- script.js
+++ script.js
@@ -14,6 +14,26 @@ function calculateAndDisplay(){
     // Display the Results in the Html
 
     document.getElementById('tipResult').innerText = 'Tip: $' + tip.toFixed(2)
-    document.getElementById('totalBillResult').innerText = 'Tip: $' + totalBill.toFixed(2)
-    document.getElementById('amtPerPersonResult').innerText = 'Tip: $' + amtPerPers.toFixed(2)
+    document.getElementById('totalBillResult').innerText = 'Total Bill: $' + totalBill.toFixed(2)
+    document.getElementById('amtPerPersonResult').innerText = 'Amount Per Person: $' + amtPerPers.toFixed(2)
+}
+
+// Helper function to calculate tip based on bill amount and service quality
+
+function calculateTip(bill, quality){
+    if(quality === 'Great'){
+        return bill * .2
+    } else if (quality === 'Good'){
+        return bill * .15
+    } else {
+        return bill * .10
+    }
+}
+
+function calculateTotalBill(bill, tip){
+    return bill + tip
+}
+
+function calcAmtPerPers(totalBill, numOfPeople){
+    return totalBill / numOfPeople
 }
